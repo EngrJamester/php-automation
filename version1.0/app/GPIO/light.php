bf618df3c92d536e915e1539c65c9188877e39c3
Go <jamester.c.go@accenture.com>
2018-11-08 08:12:25 +0800

    Light changes\

--- version1.0/app/GPIO/light.php
+++ version1.0/app/GPIO/light.php
@@ -9,7 +9,7 @@
         echo "On";
 
         //This is to turn LED Green Off
-        $off_green = shell_exec("/usr/local/bin/gpio -g write 22 0");
+        $off_green = shell_exec("/usr/local/bin/gpio -g write 27 0");
         $gpio_green_off = shell_exec($off_green);
         echo "Off";
     }
@@ -23,7 +23,7 @@
         echo "Off";
 
         //This is to turn LED Green On
-        $on_green = shell_exec("/usr/local/bin/gpio -g write 22 0");
+        $on_green = shell_exec("/usr/local/bin/gpio -g write 27 1");
         $gpio_green_on = shell_exec($on_green);
         echo "On";
         // echo
@@ -37,23 +37,23 @@
     if(isset($_GET['on-controller2'])){
         
         //This is to turn LED red On
-        $on = shell_exec("/usr/local/bin/gpio -g write 22 1");
+        $on = shell_exec("/usr/local/bin/gpio -g write 17 1");
         $gpio_on = shell_exec($on);
         echo "On";
 
         //This is to turn LED Green Off
-        $off_green = shell_exec("/usr/local/bin/gpio -g write 22 0");
+        $off_green = shell_exec("/usr/local/bin/gpio -g write 4 0");
         $gpio_green_off = shell_exec($off_green);
         echo "Off";
     }else if(isset($_GET['off-controller2'])){
         $name="controller2";
         require_once $_SERVER['DOCUMENT_ROOT'].'/php-automation/version1.0/app/classes/savetimer.php';
-        $off = shell_exec("/usr/local/bin/gpio -g write 22 0");
+        $off = shell_exec("/usr/local/bin/gpio -g write 17 0");
         $gpio_off = shell_exec($off);
         echo "Off";
 
         //This is to turn LED Green On
-        $on_green = shell_exec("/usr/local/bin/gpio -g write 22 0");
+        $on_green = shell_exec("/usr/local/bin/gpio -g write 4 1");
         $gpio_green_on = shell_exec($on_green);
         echo "On";
         echo
@@ -67,12 +67,12 @@
     if(isset($_GET['on-controller3'])){
   
         //This is to turn Red LED On
-        $on = shell_exec("/usr/local/bin/gpio -g write 22 1");
+        $on = shell_exec("/usr/local/bin/gpio -g write 18 1");
         $gpio_on = shell_exec($on);
         echo "On";
 
         //This is to turn LED Green Off
-        $off_green = shell_exec("/usr/local/bin/gpio -g write 22 0");
+        $off_green = shell_exec("/usr/local/bin/gpio -g write 23 0");
         $gpio_green_off = shell_exec($off_green);
         echo "Off";
     }else if(isset($_GET['off-controller3'])){
@@ -80,12 +80,12 @@
         require_once $_SERVER['DOCUMENT_ROOT'].'/php-automation/version1.0/app/classes/savetimer.php';
 
         //This is to turn LED Red Off
-        $off = shell_exec("/usr/local/bin/gpio -g write 22 0");
+        $off = shell_exec("/usr/local/bin/gpio -g write 18 0");
         $gpio_off = shell_exec($off);
         echo "Off";
 
          //This is to turn LED Green On
-         $on_green = shell_exec("/usr/local/bin/gpio -g write 22 0");
+         $on_green = shell_exec("/usr/local/bin/gpio -g write 23 1");
          $gpio_green_on = shell_exec($on_green);
          echo "On";
         echo
@@ -98,24 +98,26 @@
 
     if(isset($_GET['on-controller4'])){
   
-        $on = shell_exec("/usr/local/bin/gpio -g write 22 1");
+        $on = shell_exec("/usr/local/bin/gpio -g write 24 1");
         $gpio_on = shell_exec($on);
         echo "On";
 
         //This is to turn LED Green Off
-        $off_green = shell_exec("/usr/local/bin/gpio -g write 22 0");
+        $off_green = shell_exec("/usr/local/bin/gpio -g write 25 0");
         $gpio_green_off = shell_exec($off_green);
         echo "Off";
     }else if(isset($_GET['off-controller4'])){
         $name="controller4";
         require_once $_SERVER['DOCUMENT_ROOT'].'/php-automation/version1.0/app/classes/savetimer.php';
-        $off = shell_exec("/usr/local/bin/gpio -g write 22 0");
+
+        //This is to turn LED Red Off
+        $off = shell_exec("/usr/local/bin/gpio -g write 24 0");
         $gpio_off = shell_exec($off);
         echo "Off";
 
         
          //This is to turn LED Green On
-        $on_green = shell_exec("/usr/local/bin/gpio -g write 22 0");
+        $on_green = shell_exec("/usr/local/bin/gpio -g write 25 1");
         $gpio_green_on = shell_exec($on_green);
          echo "On";
         echo
@@ -128,79 +130,96 @@
 
     if(isset($_GET['on-controller5'])){
   
-        $on = shell_exec("/usr/local/bin/gpio -g write 22 1");
+        //This is to turn LED Red On
+        $on = shell_exec("/usr/local/bin/gpio -g write 12 1");
         $gpio_on = shell_exec($on);
         echo "On";
 
         //This is to turn LED Green Off
-        $off_green = shell_exec("/usr/local/bin/gpio -g write 22 0");
+        $off_green = shell_exec("/usr/local/bin/gpio -g write 16 0");
         $gpio_green_off = shell_exec($off_green);
         echo "Off";
     }else if(isset($_GET['off-controller5'])){
         $name="controller5";
         require_once $_SERVER['DOCUMENT_ROOT'].'/php-automation/version1.0/app/classes/savetimer.php';
-        $off = shell_exec("/usr/local/bin/gpio -g write 22 0");
+
+        //this is to turn on LED Red Off
+        $off = shell_exec("/usr/local/bin/gpio -g write 12 0");
         $gpio_off = shell_exec($off);
         echo "Off";
-        echo
-        '<script type="text/javascript">
 
-        alert("TIMER SAVED!");
+         //This is to turn LED Green On
+         $off_green = shell_exec("/usr/local/bin/gpio -g write 16 1");
+         $gpio_green_off = shell_exec($off_green);
+         echo "Off";
+        // echo
+        // '<script type="text/javascript">
 
-        </script>';
+        // alert("TIMER SAVED!");
+
+        // </script>';
     }
 
     if(isset($_GET['on-controller6'])){
-  
-        $on = shell_exec("/usr/local/bin/gpio -g write 22 1");
+        //This is to turn LED Red On
+        $on = shell_exec("/usr/local/bin/gpio -g write 20 1");
         $gpio_on = shell_exec($on);
         echo "On";
 
         //This is to turn LED Green Off
-        $off_green = shell_exec("/usr/local/bin/gpio -g write 22 0");
+        $off_green = shell_exec("/usr/local/bin/gpio -g write 21 0");
         $gpio_green_off = shell_exec($off_green);
         echo "Off";
     }else if(isset($_GET['off-controller6'])){
         $name="controller6";
         require_once $_SERVER['DOCUMENT_ROOT'].'/php-automation/version1.0/app/classes/savetimer.php';
-        $off = shell_exec("/usr/local/bin/gpio -g write 22 0");
+
+        //This is to turn LED Red Off
+        $off = shell_exec("/usr/local/bin/gpio -g write 20 0");
         $gpio_off = shell_exec($off);
         echo "Off";
 
          //This is to turn LED Green On
-        $on_green = shell_exec("/usr/local/bin/gpio -g write 22 0");
+        $on_green = shell_exec("/usr/local/bin/gpio -g write 21 1");
         $gpio_green_on = shell_exec($on_green);
         echo "On";
-        echo
-        '<script type="text/javascript">
+        // echo
+        // '<script type="text/javascript">
 
-        alert("TIMER SAVED!");
+        // alert("TIMER SAVED!");
 
-        </script>';
+        // </script>';
     }
 
     if(isset($_GET['on-controller7'])){
-  
-        $on = shell_exec("/usr/local/bin/gpio -g write 22 1");
+        //This is to turn LED Red On
+        $on = shell_exec("/usr/local/bin/gpio -g write 26 1");
         $gpio_on = shell_exec($on);
         echo "On";
 
         //This is to turn LED Green Off
-        $off_green = shell_exec("/usr/local/bin/gpio -g write 22 0");
+        $off_green = shell_exec("/usr/local/bin/gpio -g write 19 0");
         $gpio_green_off = shell_exec($off_green);
         echo "Off";
     }else if(isset($_GET['off-controller7'])){
         $name="controller7";
         require_once $_SERVER['DOCUMENT_ROOT'].'/php-automation/version1.0/app/classes/savetimer.php';
-        $off = shell_exec("/usr/local/bin/gpio -g write 22 0");
+
+        //this is to turn LED Red Off
+        $off = shell_exec("/usr/local/bin/gpio -g write 26 0");
         $gpio_off = shell_exec($off);
         echo "Off";
-        echo
-        '<script type="text/javascript">
 
-        alert("TIMER SAVED!");
+         //This is to turn LED Green On
+         $off_green = shell_exec("/usr/local/bin/gpio -g write 19 1");
+         $gpio_green_off = shell_exec($off_green);
+         echo "Off";
+        // echo
+        // '<script type="text/javascript">
 
-        </script>';
+        // alert("TIMER SAVED!");
+
+        // </script>';
     }
 
 ?>
